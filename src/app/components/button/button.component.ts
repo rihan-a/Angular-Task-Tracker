66d02e48927ed2bdfa290db10684d0f253e2e41f
rihan_a <a.rihan@live.com>
2023-07-28 15:29:47 +0200

    add click event handler

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
+  @Input()
+  text!: string;
+  @Input()
+  color!: string;
 
+  @Output() btnClick = new EventEmitter();
+
+  onClick() {
+    this.btnClick.emit();
+  }
 }
